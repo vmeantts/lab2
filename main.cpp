b57c33a9a51d3ee53ebf28ebc1142b3dee3fb906
vmeantts <ivm261107@gmail.com>
2026-05-17 14:22:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include "Table.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int rows;
+    int cols;
+    int value;
+
+    cout << "введите количество строк: ";
+    cin >> rows;
+
+    cout << "введите количество столбцов: ";
+    cin >> cols;
+
+    while (rows <= 0 || cols <= 0)
+    {
+        cout << "ошибка. введите заново: ";
+        cin >> rows >> cols;
+    }
+
+    Table table(rows, cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << "введите элемент [" << i << "][" << j << "]: ";
+            cin >> value;
+
+            table.set_value(i, j, value);
+        }
+    }
+
+    cout << "\nтаблица:\n";
+    table.print();
+
+    cout << "\nсреднее: " << table.average() << endl;
+
+    return 0;
+}
